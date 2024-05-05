251dcab98e003b16f3bc37016adc4a8bf3a7a9bb
lucas-laza <laza.lucas15@gmail.com>
2024-05-05 20:07:00 +0200

    connexion back front

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,5 +1,30 @@
+import express from "express";
 import { Article } from "./Article";
 import { getNewDataSource } from "./config/database";
+
+const app = express();
+const port = 3030; // Remplacez xxxx par le port sur lequel vous souhaitez que votre serveur écoute
+
+app.use(express.json());
+
+// Middleware pour ajouter les en-têtes CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Autorise les requêtes de n'importe quelle origine
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Autorise les méthodes de requête spécifiées
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Autorise les en-têtes spécifiés
+  next();
+});
+
+app.get("/api/articles", async (req, res) => {
+  try {
+    let articles: any[] = []; // Initialise articles avec un tableau vide
+    // const articles = await Article.getAll(); // Supposons que vous avez une méthode statique dans votre modèle Article pour récupérer tous les articles
+    res.json({ articles });
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 async function main() {
   const dataSource = await getNewDataSource("./sqlite.db");
@@ -8,8 +33,9 @@ async function main() {
   await Article.createBaseArticles();
   console.log("Successfully created articles.");
 
-  // start HTTP server…
-  console.log("🚀 Server listening on port xxxx.");
+  app.listen(port, () => {
+    console.log(`🚀 Server listening on port ${port}.`);
+  });
 }
 
 main();
--- web-app/src/lib/http.ts
+++ web-app/src/lib/http.ts
@@ -1,9 +1,9 @@
 export async function sendGetRequest(
   url: string
 ): Promise<Record<string, unknown>> {
-  const response = await fetch(url);
+  const response = await fetch("http://localhost:3030" + url);
   if (!response.ok) {
-    throw new Error(HTTP error! Status: ${response.status});
+    throw new Error(`HTTP error! Status: ${response.status}`);
   }
   const data = await response.json();
   return data;
